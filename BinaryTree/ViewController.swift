5e8dfcfb0f9f8a4833f948dfc5bf7b66cd5854fe
Manish Singh <mmmanishs@gmail.com>
2018-03-05 20:33:01 -0500

    Correct

--- BinaryTree/ViewController.swift
+++ BinaryTree/ViewController.swift
@@ -11,15 +11,15 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var scrollview: UIScrollView!
     var rootNode = Node(value: 10, completion: nil)
-    let startRect = CGRect(x: 100, y: 50, width: 50, height: 50)
+    let startRect = CGRect(x: 100, y: 50, width: 50, height: 100)
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.scrollview.contentSize = CGSize(width: 1500, height: 1500)
-        self.scrollview.contentOffset = CGPoint(x: 1000, y: 750)
+        self.scrollview.contentOffset = CGPoint(x: 750, y: 50)
         rootNode.center = self.scrollview.contentOffset
         DispatchQueue.main.async {
-            self.buildBinaryTree(withRootNode: self.rootNode, numberOfNodes: 100)
+            self.buildBinaryTree(withRootNode: self.rootNode, numberOfNodes: 20)
             let renderTree = RenderTree(scrollView: self.scrollview)
             renderTree.drawTree(node: self.rootNode)
         }
